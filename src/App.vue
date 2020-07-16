4cfae2caa81f1c1ec57e44385067f34bb16e8f3c
anhpham <anh.pham@belive.sg>
2020-07-16 11:01:09 +0700

    new cli

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,32 @@
 <template>
-    <div id="app">
-        <Navbar></Navbar>
-        <div class="main main-page belive-main-page" role="main">
-            <router-view
-                class="view-route-partial"
-                id="view-route-partial"
-            />
-        </div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Navbar from "./sections/navbar"
-import Footer from "./sections/footer"
-
-export default {
-    name: "App",
-    components: {
-        Navbar,
-        Footer,
-    },
-    mixins: [],
-    data() {
-        return {
-            keyRouter: 0,
-        }
-    },
-    watch: {},
-    computed: {
-
-    },
-    created() {
-    },
-    mounted() {
-
-    },
-    methods: {
-
-    },
-    beforeDestroy() {
-    },
-}
-</script>
-
 <style lang="scss">
-@import "assets/scss/style";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import CheckPlayer from "../pages/CheckPlayer";
-import MultipleStreams from "../pages/MultipleStreams";
-import NotFoundView from "../pages/404";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: "/multiple-streams",
-        name: "MultipleStreams",
-        component: MultipleStreams
-    },
-    {
-        path: "/",
-        name: "CheckPlayer",
-        component: CheckPlayer
-    },
-    { path: '*', name: 'NotFound', component: NotFoundView },
-];
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes
-});
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
 
-export default router;
+export default router
